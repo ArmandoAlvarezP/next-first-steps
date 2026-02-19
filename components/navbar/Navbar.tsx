21012710df6293f6573a92988c120fc11b0eb10b
ArmandoAlvarezP <armando.alvarez.gkm@hotmail.com>
2026-02-18 21:27:39 -0600

    Se agrega ActiveLink (fin de sección de introducción)

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,15 +1,33 @@
+import Link from "next/link"
+import { HomeIcon } from "@primer/octicons-react"
+import { ActiveLink } from "../active-link/ActiveLink"
 
-export const Navbar = () => {
+const navItems = [
+    { path: '/about', text: 'About'},
+    { path: '/pricing', text: 'Pricing'},
+    { path: '/contact', text: 'Contact'},
+]
+
+export const Navbar = async () => {
     return (
         <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-            <span>Home</span>
+            
+            <Link className="flex items-center" href="/">
+                <HomeIcon className="mr-2"/>
+                <span>Home</span>
+            </Link>
 
             <div className="flex flex-1"></div>
-            
-            <a className="mr-2" href="/about">About</a>
-            <a className="mr-2" href="/pricing">Pricing</a>
-            <a className="mr-2" href="/contact">Contact</a>
 
+            {
+                navItems.map( navItem => (
+                    <ActiveLink key={navItem.path} {...navItem}/>
+                ))
+            }
+
+            
+            {/* <Link className="mr-2" href="/pricing">Pricing</Link>
+            <Link className="mr-2" href="/contact">Contact</Link> */}
         </nav>
     )
 }
